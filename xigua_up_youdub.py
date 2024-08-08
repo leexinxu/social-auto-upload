66c582080f054f0e7b47b89c13d3b92eb3cd2166
lxx <leexinxu@163.com>
2024-08-08 21:29:17 +0800

    新路径

--- xigua_up_youdub.py
+++ xigua_up_youdub.py
@@ -117,7 +117,7 @@ def check_up(src_dir, account_file):
 # 启动自动上传
 BASE_DIR = Path(__file__).parent.resolve()
 account_file = Path(BASE_DIR / "xigua_uploader" / "account.json")
-src_dir = '/Volumes/Data/AI/YouDub-webui/videos'
+src_dir = '/Volumes/Data/AI/YouDub-webui/videos_20240808'
 check_up(src_dir, account_file)
 
 
